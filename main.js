574fbcaa22058f6c4283e641ee87a0f3f8c9a075
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-06-21 23:56:31 +0200

    Add semicolons to every statement
    require(./renderer) was also broken last commit and is now fixed.

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const {app, BrowserWindow, ipcMain} = require("electron")
-const path = require('path')
+const {app, BrowserWindow, ipcMain} = require("electron");
+const path = require('path');
 
 // Paths
-const settingspath = path.join(__dirname, 'settings.json')
-const indexpath = path.join(__dirname, 'app', 'index.html')
+const settingspath = path.join(__dirname, 'settings.json');
+const indexpath = path.join(__dirname, 'app', 'index.html');
 
-let settings
+let settings;
 
 function createSettings() {
     const settings =
     {
         debug: false
-    }
-    var settingsstring = JSON.stringify(settings, null, 2)
-    var fs = require("fs")
-    fs.writeFile(settingspath, settingsstring)
-    return settings
-}
+    };
+    var settingsstring = JSON.stringify(settings, null, 2);
+    var fs = require("fs");
+    fs.writeFile(settingspath, settingsstring);
+    return settings;
+};
 
 try {
-    settings = require(settingspath)
+    settings = require(settingspath);
 } catch (e) {
-    settings = createSettings()
+    settings = createSettings();
 }
   
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow () {
     // Create the browser window.
@@ -37,14 +37,14 @@ function createWindow () {
         frame: false,
         resizable: settings.debug,
         icon: path.join(__dirname, 'assets/icons/png/64x64.png')
-    })
+    });
 
     // and load the index.html of the app.
-    win.loadFile(indexpath)
+    win.loadFile(indexpath);
 
     // Open the DevTools.
     if (settings.debug)
-        win.webContents.openDevTools()
+        win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -52,33 +52,33 @@ function createWindow () {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null
-    })
-}
+    });
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 ipcMain.on('close-me', (evt, arg) => {
-  app.quit()
-})
+  app.quit();
+});
